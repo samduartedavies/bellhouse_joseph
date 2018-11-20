ab21c0d76a6e0fe4fe424cb00a3ecc17237bc817
Samuel Duarte-Davies <samduarte-davies@hotmail.com>
2018-11-20 22:14:26 +0000

    completed gulp manouvers to minify and compress css and js effectively

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minify = require('gulp-minify');
 var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 function handleError(err) {
   console.log(err.toString());
@@ -11,29 +11,55 @@ function handleError(err) {
 }
 
 sass.compiler = require('node-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('common/scss/*.scss')
+
+gulp.task('scss', function() {
+  return gulp
+    .src('common/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('common/css'));
 });
 
-gulp.task('compress', function() {
-  gulp.src('common/js/main.js')
-    .pipe(minify({
-        ext:{
-            src:'.js',
-            min:'.min.js'
+gulp.task('compress-js', function() {
+  gulp
+    .src('common/js/main.js')
+    .pipe(
+      minify({
+        ext: {
+          src: '.js',
+          min: '.min.js',
         },
         exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '.min.js']
-    }))
-    .pipe(gulp.dest('common/js/'))
+        ignoreFiles: ['.combo.js', '.min.js'],
+      })
+    )
+    .pipe(gulp.dest('common/js/'));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('common/scss/*.{sass,scss}', ['dev']);
-  gulp.watch('common/js/main.js', ['compress']);
+gulp.task('minify-css', () =>
+  gulp
+    .src('common/css/main.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('common/css'))
+);
+
+gulp.task('browsers', () =>
+  gulp
+    .src('common/css/main.css')
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest('common/css'))
+);
+
+gulp.task('watch', function() {
+  gulp.watch('common/scss/*.{sass,scss}', ['scss']);
+  gulp.watch('common/css/main.css', ['browsers']);
+  gulp.watch('common/css/main.css', ['minify-css']);
+  gulp.watch('common/js/main.js', ['compress-js']);
 });
 
 gulp.task('dev', ['sass']);
