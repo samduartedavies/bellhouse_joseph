9ed0b4c5156ca51c3fac6422a0ef61f46ae72953
Samuel Duarte-Davies <samduarte-davies@hotmail.com>
2018-11-22 22:15:30 +0000

    navigation has a dropdown

--- common/inc/navigation.php
+++ common/inc/navigation.php
@@ -7,6 +7,10 @@
       </li>
       <li>
         <a href="/case-studies">Case Studies</a>
+        <ul class="menu__dropdown">
+          <li><a href="/case-studies/case-study-a">Case Study A</a></li>
+          <li><a href="/case-studies/case-study-b">Case Study B</a></li>
+        </ul>
       </li>
       <li>
         <a href="/about-us">About us</a>
